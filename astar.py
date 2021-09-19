6be19fa8039d07d334a4ef3bfeccb90e001dc0ef
Nitish Poddar <npoddar2@illinois.edu>
2021-09-19 16:36:07 -0500

    Debugged astar

--- astar.py
+++ astar.py
@@ -1,66 +1,55 @@
 from queue import PriorityQueue
 
-LEFT_MAX = -99
-RIGHT_MAX = 100
-UP_MAX = 99
-BASE_X = 99
-BASE_Y = 99
+LEFT_MAX = -4
+RIGHT_MAX = 5
+UP_MAX = 4
+BASE_X = 4
+BASE_Y = 4
 
-GRID_ROWS = 100
-GRID_COLS = 200
+GRID_ROWS = 5
+GRID_COLS = 10
+
+PATH_THRESHOLD = 3
 
 def updateGrid():
-    # Placeholder for actual method
-    pass
+    # TODO: Placeholder for actual method
+    grid = [[0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,1,1,1,1,1,1,1,1,0],
+            [0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0]]
+
+    return grid
 
 class Node:
-    def __init__(self, parent=None, position=None):
+    def __init__(self, parent=None, pos=None):
         self.parent = parent
-        self.position = position
+        self.pos = pos
 
         self.g = 0
         self.h = 0
         self.f = 0
 
     def __eq__(self, other):
-        return self.position == other.position
-        
-def moveToPoint(x, y):
-    while x != 0 and y != 0:
-        grid = updateGrid()
+        if other == None:
+            return False
+        return self.pos == other.pos
 
-        remX, remY = 0, 0
-        if x < LEFT_MAX:
-            remX = x + LEFT_MAX
-            x = LEFT_MAX
-        elif x > RIGHT_MAX:
-            remX = x-RIGHT_MAX
-            x = RIGHT_MAX
-        
-        x = BASE_X+x
-
-        if y < 0:
-            remY = -y
-            # turn around and call function again
-        elif y > UP_MAX:
-            remY = y-UP_MAX
-            y = UP_MAX
-        
-        y = BASE_Y-y
-
-        path = search(grid, (BASE_X, BASE_Y), (x, y))
-        # TODO: move on path THRESHOLD steps at a time
-
-        x = remX
-        y = remY
+    def __hash__(self):
+        return hash(self.pos)
+    
+    def __lt__(self, other):
+        if other == None:
+            return False
+        return self.f < other.f
 
 def getPath(end, grid):
     path = []
     
     curr = end
     while curr != None:
-        path.append(curr.position)
-        current = current.parent
+        path.append(curr.pos)
+        curr = curr.parent
 
     path.reverse()
     return path
@@ -81,7 +70,7 @@ def search(grid, start, end):
 
     adj = [[-1, 0],[0, -1],[1, 0],[0, 1]]
 
-    while len(q) > 0:
+    while not q.empty():
         f, curr = q.get()
 
         visited.add(curr)
@@ -90,17 +79,72 @@ def search(grid, start, end):
             return getPath(curr, grid)
 
         for pos in adj:
-            newX, newY = curr.position[0] + pos[0], curr.position[1] + pos[1]
-
-            if newX >= GRID_COLS or newX < 0 or newY >= GRID_ROWS or newY < 0 or grid[newX][newY] == 1:
+            newX, newY = curr.pos[0] + pos[0], curr.pos[1] + pos[1]
+            if newX >= GRID_COLS or newX < 0 or newY >= GRID_ROWS or newY < 0 or grid[newY][newX] == 1:
                 continue
 
             newNode = Node(curr, (newX, newY))
             if newNode in visited:
                 continue
 
-            newNode.g = curr.g + 1
-            newNode.h = heuristic(newNode.position, end.position)
+            newNode.g = curr.g+1
+            newNode.h = heuristic(newNode.pos, end.pos)
             newNode.f = newNode.g + newNode.h
 
             q.put((newNode.f, newNode))
+
+def moveOnPath(path, target):
+    prev = None
+    totalMoves = (0, 0)
+    for idx, curr in enumerate(path):
+        if idx == PATH_THRESHOLD or prev == target:
+            break
+
+        if prev == None:
+            prev = curr
+            continue
+        
+        move = (curr[0]-prev[0], curr[1]-prev[1])
+        totalMoves = (totalMoves[0]+move[0], totalMoves[1]+move[1])
+        # TODO: move the car in the given direction
+
+        prev = curr
+
+    target = (target[0]-totalMoves[0], target[1]-move[1])
+    return target
+
+def moveToPoint(x, y):
+    while x != 0 and y != 0:
+        remX, remY = 0, 0
+        if x < LEFT_MAX:
+            remX = x + LEFT_MAX
+            x = LEFT_MAX
+        elif x > RIGHT_MAX:
+            remX = x-RIGHT_MAX
+            x = RIGHT_MAX
+        
+        x = BASE_X+x
+
+        if y < 0:
+            y = -y
+            x = -x
+            # TODO: make 180 turn and continue
+        elif y > UP_MAX:
+            remY = y-UP_MAX
+            y = UP_MAX
+
+        y = BASE_Y-y
+
+        print(x, y, remX, remY)
+        base = (BASE_X, BASE_Y)
+        while (x, y) != base:
+            grid = updateGrid()
+            path = search(grid, (BASE_X, BASE_Y), (x, y))
+            x, y = moveOnPath(path, (x, y))
+            print(path, x, y)
+
+        x = remX
+        y = remY
+
+if __name__ == "__main__":
+    moveToPoint(1, 5)
