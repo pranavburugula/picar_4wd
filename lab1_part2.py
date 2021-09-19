7ab09fc00173e24226c4025560ff22500733cf5d
Pranav Burugula <pranav.burugula@gmail.com>
2021-09-19 17:05:32 -0500

    Added turn angle and move distance

--- lab1_part2.py
+++ lab1_part2.py
@@ -2,8 +2,11 @@ import picar_4wd as fc
 import numpy as np
 import signal
 import sys
+import time
 
 ANGLE_RANGE = 180
+TURN_RADIUS = 13.97
+WHEEL_RADIUS = 3.175
 STEP = 18
 us_step = STEP
 angle_distance = [0,0]
@@ -37,12 +40,36 @@ def scan_step():
     else:
         return []
 
+def move_distance(dist, power):
+    dist *= 2.54
+
+    cur_dist = 0
+    time.sleep(0.5)
+    fc.forward(power)
+    while cur_dist < dist:
+        time.sleep(0.002)
+        cur_dist += fc.speed_val() * 0.002
+    fc.stop()
+
+def turn_angle(angle, power):
+    angle_rad = np.radians(np.abs(angle))
+    arc_length = angle_rad * TURN_RADIUS
+
+    left_dist = 0
+    right_dist = 0
+    time.sleep(0.5)
+    fc.turn_right(power if angle >= 0 else -power)
+    while np.abs(left_dist) < arc_length and np.abs(right_dist) < arc_length:
+        time.sleep(0.002)
+        left_dist += fc.left_rear_speed() * 0.002
+        right_dist += fc.right_rear_speed() * 0.002
+    fc.stop()
 
 def printA(grid):
     for row in grid:
         print(row)
 
-def sig_handler(signal, frame):
+def scan_sig_handler(signal, frame):
     global scans
     # scan_arr = np.array(scans)
     # np.save(scan_arr, "maps")
@@ -54,64 +81,75 @@ def sig_handler(signal, frame):
     #         file.write(str(row))
 
     sys.exit(0)
-    
-signal.signal(signal.SIGINT, sig_handler)
 
-angles = np.array([i for i in range(-int(ANGLE_RANGE / 2), int(ANGLE_RANGE / 2), STEP)])
-angles = np.radians(angles)
-cosines = np.cos(angles)
-sines = np.sin(angles)
-while 1:
-    scan = np.array(scan_step())
-    grid = np.zeros((100, 200))
-    # print(cosines)
-    if len(scan) > 0: 
-        # scan[scan > 100] = -1
-        # scan[scan < 0] = -1
+def move_sig_handler(signal, frame):
+    fc.stop()
+    sys.exit(0)
 
-        # angles = output[1]
-        # print(scan)
-        # print(angles)
-        last = 0
-        last_row = -1
-        last_col = -1
-        for i in range(len(scan)):
-            # print(len(scan), len(cosines))
-            row = int(100 - scan[i] * cosines[i])
-            col = int(100 + scan[i] * sines[i])
+def scan_relative_map():
+    signal.signal(signal.SIGINT, scan_sig_handler)
 
-            if row < 0 or col < 0 or col > 200:
-                continue
-            
-            # print(row, col, scan[i])
-            if scan[i] > 0 and scan[i] <= 100:
-                grid[row - 1][col - 1] = 1
+    angles = np.array([i for i in range(-int(ANGLE_RANGE / 2), int(ANGLE_RANGE / 2), STEP)])
+    angles = np.radians(angles)
+    cosines = np.cos(angles)
+    sines = np.sin(angles)
+    while 1:
+        scan = np.array(scan_step())
+        grid = np.zeros((100, 200))
+        # print(cosines)
+        if len(scan) > 0: 
+            # scan[scan > 100] = -1
+            # scan[scan < 0] = -1
+
+            # angles = output[1]
+            # print(scan)
+            # print(angles)
+            last = 0
+            last_row = -1
+            last_col = -1
+            for i in range(len(scan)):
+                # print(len(scan), len(cosines))
+                row = int(100 - scan[i] * cosines[i])
+                col = int(100 + scan[i] * sines[i])
+
+                if row < 0 or col < 0 or col > 200:
+                    continue
                 
-                # fill in line between this and last point as ones
-                if last == 1:
-                    if col == last_col:
-                        for i in range(last_row, row):
-                            grid[i][col - 1] = 1
-                    else:
-                        slope = (row - last_row) / (col - last_col)
-                        for i in range(last_col, col):
-                            grid[int(last_row + slope * (i - last_col)) - 1][i] = 1
+                # print(row, col, scan[i])
+                if scan[i] > 0 and scan[i] <= 100:
+                    grid[row - 1][col - 1] = 1
+                    
+                    # fill in line between this and last point as ones
+                    if last == 1:
+                        if col == last_col:
+                            for i in range(last_row, row):
+                                grid[i][col - 1] = 1
+                        else:
+                            slope = (row - last_row) / (col - last_col)
+                            for i in range(last_col, col):
+                                grid[int(last_row + slope * (i - last_col)) - 1][i] = 1
 
-                
-                last = 1
-                last_row = row
-                last_col = col
-            else:
-                last = 0
-            # else:
-            #     grid[row - 1][col - 1] = 0
+                    
+                    last = 1
+                    last_row = row
+                    last_col = col
+                else:
+                    last = 0
+                # else:
+                #     grid[row - 1][col - 1] = 0
 
 
-        # print(np.where(grid == 1))
-        # printA(grid)
-        scans.append(grid)
+            # print(np.where(grid == 1))
+            # printA(grid)
+            scans.append(grid)
 
-        
+if __name__=="__main__":
+    signal.signal(signal.SIGINT, move_sig_handler)
+    fc.left_rear_speed.start()
+    fc.right_rear_speed.start()
+    move_distance(20, 2)
+    turn_angle(-90, 2)
+    move_distance(5, 2)
 
-        
-
+    fc.left_rear_speed.deinit()
+    fc.right_rear_speed.deinit()
